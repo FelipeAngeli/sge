708f93216ea796a7cb8e493cf1364396d46bd095
Felipe Angeli <59125006+FelipeAngeli@users.noreply.github.com>
2024-12-04 11:04:30 -0300

    ajuste AIResult na view

--- app/views.py
+++ app/views.py
@@ -7,14 +7,19 @@ from . import metrics
 
 @login_required(login_url='login')
 def home(request):
+    # Recupera os dados de métricas
     product_metrics = metrics.get_product_metrics()
     sales_metrics = metrics.get_sales_metrics()
     graphic_product_category_metric = metrics.get_graphic_product_category_metric()
     graphic_product_brand_metric = metrics.get_graphic_product_brand_metric()
     daily_sales_data = metrics.get_daily_sales_data()
     daily_sales_quantity_data = metrics.get_daily_sales_quantity_data()
-    ai_result = AIResult.objects.first().result
 
+    # Verifica se há um registro em AIResult antes de tentar acessar o atributo result
+    ai_result = AIResult.objects.first()
+    ai_result_value = ai_result.result if ai_result else "Nenhum resultado disponível."
+
+    # Contexto para o template
     context = {
         'product_metrics': product_metrics,
         'sales_metrics': sales_metrics,
@@ -22,7 +27,7 @@ def home(request):
         'product_count_by_brand': json.dumps(graphic_product_brand_metric),
         'daily_sales_data': json.dumps(daily_sales_data),
         'daily_sales_quantity_data': json.dumps(daily_sales_quantity_data),
-        'ai_result': ai_result,
+        'ai_result': ai_result_value,
     }
 
     return render(request, 'home.html', context)
